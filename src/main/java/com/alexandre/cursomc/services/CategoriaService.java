21886902ed60f657473ea8721f94288859037ce1
alex241262 <alexandreferreira168@gmail.com>
2020-10-21 12:40:39 -0400

    Personalizando o retorno da validacao

--- src/main/java/com/alexandre/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandre/cursomc/services/CategoriaService.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alexandre.cursomc.domain.Categoria;
+import com.alexandre.cursomc.dto.CategoriaDTO;
 import com.alexandre.cursomc.repositories.CategoriaRepository;
 import com.alexandre.cursomc.services.exceptions.DataIntegrityException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
@@ -43,4 +47,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 		}
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
